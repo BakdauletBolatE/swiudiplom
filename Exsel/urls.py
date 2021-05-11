28885127914047c8464128aba3da4e99892bbe43
Баклаулет Болат <baklauletbolat@MacBook-Air-Baklaulet.local>
2021-05-11 19:55:39 +0600

    fix

--- Exsel/urls.py
+++ Exsel/urls.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import  HttpResponse
-from newdiplom.models import SwiuDiplom2020
+# from django.http import  HttpResponse
+# from newdiplom.models import SwiuDiplom2020
 
-def deleteAll(request):
-    SwiuDiplom2020.objects.all().delete()
-    return HttpResponse('Succes')
+# def deleteAll(request):
+#     SwiuDiplom2020.objects.all().delete()
+#     return HttpResponse('Succes')
 
 
 urlpatterns = [
-    path('delete/',deleteAll),
+    # path('delete/',deleteAll),
     path('', admin.site.urls),
 ]
 
